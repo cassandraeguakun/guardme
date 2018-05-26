6dc581302c71ea8cf1afd7dc0118e58061e72f05
Ashok <er.ashok23@gmail.com>
2018-05-26 20:21:37 +0530

    JobsController.php update for job edits
    Users can edit, delete, pause job by clicking icons on job list

--- local/app/Http/Controllers/JobsController.php
+++ local/app/Http/Controllers/JobsController.php
@@ -86,6 +86,107 @@ class JobsController extends Controller {
         return view('jobs.my', compact('my_jobs','editprofile'));
     }
 
+	
+	////////////////////////
+
+    public function savedJobs() {
+        $userid = Auth::user()->id;
+        $editprofile = User::where('id',$userid)->get();
+        $my_jobs =  DB::select('select security_jobs.* from security_jobs, saved_jobs where saved_jobs.job_id = security_jobs.id and saved_jobs.user_id = '.$userid);
+        return view('jobs.saved', compact('my_jobs','editprofile'));
+    }
+
+    function editJob($id){
+        //User::findOrFail(1);
+        $job = Job::find($id);
+        if(empty($job)){
+             \Session::flash('message', 'Please select job');
+               return \Redirect::to('/jobs/my/'); 
+        }
+        $all_security_categories = SecurityCategory::get();
+        $all_business_categories = Businesscategory::get();
+        return view('jobs.editJobs', compact('job', 'all_security_categories', 'all_business_categories'));
+      
+    }
+    function editJobPost(Request $request){
+       $data = $request->all();
+
+       $job_id = $data['job_id'];
+
+                if($job_id != ''){
+                     \Session::flash('message', 'Invalid Jobs!!');
+                               return \Redirect::to('/jobs/editJob/'.$job_id); 
+                }
+
+
+
+            $datavalue = array(
+            'title'=>$data['title'],
+            'description'=>$data['description'],
+            'address_line1'=>$data['line1'],
+            'address_line2'=>$data['line2'],
+            'address_line3'=>$data['line3'],
+            'city_town'=>$data['town'],
+            'post_code'=>$data['postcode'],
+             'country'=>$data['country'],
+           'security_category_id'=>$data['security_category'],
+            'business_category_id'=>$data['business_category'],
+            'updated_at'=>Auth::user()
+            );
+
+         $check = Job::where('id',$job_id)->update($datavalue);
+            if($check>0)
+            {
+              \Session::flash('message', 'Job Successfully Updated...');
+               return \Redirect::to('/jobs/editJob/'.$job_id); 
+            }
+            else
+            {
+              
+              //return \Redirect::back()->with('message', 'Action Failed...Please Try Again!!!');
+              \Session::flash('message', 'Action Failed...Please Try Again!!!');
+               return \Redirect::to('/jobs/editJob/'.$job_id); 
+            }
+
+        // \Session::flash('message', 'Account Information Successfully Updated...');
+        //        return \Redirect::to('/jobs/editJob/'.$job_id);  
+       //      echo '<pre>';
+       // print_r($datavalue);
+    }
+
+    function deleteJob($id){
+         $id = Job::find( $id );
+         $flag = $id->delete();
+         if($flag){
+                     return redirect()->to('/jobs/my')->with('success', 'Job Deleted Successfully!');
+            }else{
+                    return Redirect()->to('/jobs/my')->with('error', 'Delete Failed');
+            }
+    }
+
+    function pauseJob($id){
+         $job = Job::find( $id );
+         $check = Job::where('id',$id)->update(array('status'=>'0'));
+         if($check){
+                     return redirect()->to('/jobs/my')->with('success', 'Job Pause Successfully!');
+            }else{
+                    return Redirect()->to('/jobs/my')->with('error', 'Pause Operation Failed');
+            }
+    }
+
+     function activeJob($id){
+         $job = Job::find( $id );
+         $check = Job::where('id',$id)->update(array('status'=>'1'));
+         if($check){
+                     return redirect()->to('/jobs/my')->with('success', 'Job Pause Successfully!');
+            }else{
+                    return Redirect()->to('/jobs/my')->with('error', 'Pause Operation Failed');
+            }
+    }
+///////////////////////////
+
+	
+	
     public function savedJobs() {
         $userid = Auth::user()->id;
         $editprofile = User::where('id',$userid)->get();
